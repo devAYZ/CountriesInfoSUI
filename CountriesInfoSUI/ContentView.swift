ece9cfe61787911915d6e078ece3d45309be8c6a
devAYZ <fatokimiemman@gmail.com>
2024-09-07 14:03:15 +0100

    Initial Commit

---
+++ CountriesInfoSUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CountriesInfoSUI
+//
+//  Created by Ayokunle Pro on 9/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CountriesInfoSUI/CountriesInfoSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesInfoSUIApp.swift
+//  CountriesInfoSUI
+//
+//  Created by Ayokunle Pro on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CountriesInfoSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
